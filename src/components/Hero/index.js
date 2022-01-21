a7f79cff24752dbcabb547681fe4af28eef35deb
07178.ouch.liu <ouch.liu@xuenn.com>
2022-01-21 14:34:03 +0800

    1. Fix broken pages. 2. Update configurations.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,12 +4,6 @@ import Translate, { translate } from "@docusaurus/Translate";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Link from "@docusaurus/Link";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLinkedin,
-  faGithub,
-  faWeixin,
-} from "@fortawesome/free-brands-svg-icons";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
 //import useFollowers from "./useFollowers";
@@ -38,108 +32,7 @@ function Hero() {
 
   return (
     <animated.div className={styles.hero}>
-      <div className={styles.bloghome__intro}>
-        <animated.div style={animatedTexts[0]} className={styles.hero_text}>
-          <Translate description="hero greet">Hello! 我是</Translate>
-          <span className={styles.intro__name}>
-            <Translate description="my name">峰华</Translate>
-          </span>
-        </animated.div>
-        <animated.p style={animatedTexts[1]}>
-          <Translate
-            id="homepage.hero.text"
-            description="hero text"
-            values={{
-              bilibiliText: (
-                <Link to="/docs/videos">
-                  <Translate
-                    id="hompage.hero.text.bilibili"
-                    description="Bilibili docs link label"
-                  >
-                    技术视频教程、
-                  </Translate>
-                </Link>
-              ),
-              courses: (
-                <Link to="/docs/course/react-chat-ui">
-                  <Translate
-                    id="hompage.hero.text.course"
-                    description="Course link label"
-                  >
-                    实战课程、
-                  </Translate>
-                </Link>
-              ),
-              blogs: (
-                <Link to="#homepage_blogs">
-                  <Translate
-                    id="hompage.hero.text.blog"
-                    description="Blog link label"
-                  >
-                    技术博客、
-                  </Translate>
-                </Link>
-              ),
-              links: (
-                <Link to="/docs/resources">
-                  <Translate
-                    id="hompage.hero.text.link"
-                    description="Link link label"
-                  >
-                    前端资源导航、
-                  </Translate>
-                </Link>
-              ),
-              ideas: (
-                <Link to="/lifestyle">
-                  <Translate
-                    id="hompage.hero.text.idea"
-                    description="Idea link label"
-                  >
-                    想法和生活点滴
-                  </Translate>
-                </Link>
-              ),
-            }}
-          >
-            {`点击查看最新{bilibiliText}{courses}{blogs}{links}以及UP主的{ideas}。致力于帮助你以最直观、最快速的方式学会前端开发，并希望我的个人经历对你有所启发。`}
-          </Translate>
-        </animated.p>
-        {currentLocale === "zh-CN" && (
-          <animated.p style={animatedTexts[3]}>
-            <Translate id="homepage.qqgroup1" description="qq group1">
-              QQ 1 群：644722908
-            </Translate>
-            <br />
-            <Translate id="homepage.qqgroup2" description="qq group2">
-              QQ 2 群：1004912565
-            </Translate>
-          </animated.p>
-        )}
-        <SocialLinks animatedProps={animatedTexts[4]} />
-        <animated.div style={animatedTexts[2]}>
-          <Button
-            isLink
-            href={translate({
-              id: "homepage.follow.link.href",
-              message:
-                "https://space.bilibili.com/302954484?from=search&seid=1788147379248960737",
-              description: "social link bilibili or twitter",
-            })}
-          >
-            <Translate description="follow me btn text">去B站关注</Translate>
-            <Translate
-              id="homepage.followers"
-              description="followers"
-              values={{ count: (Math.round(followers) / 10000).toFixed(1) }}
-            >
-              {" {count} 万"}
-            </Translate>
-          </Button>
-        </animated.div>
-      </div>
-
-      <HeroMainImage />
+      
       {/* <animated.div
       className="bloghome__scroll-down"
       style={animatedBackground}
