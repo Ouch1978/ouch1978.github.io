9dfeabb85b43e84c39ae0b5d04837bbbb9235891
Ouch1978 <ouch1978@hotmail.com>
2022-02-04 15:11:04 +0800

    1. Add hero header.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,36 +3,31 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-<section class="hero">
-  <div class="content">
-    <h2>Hero Header</h2>
-    <p>Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non</p>
+const {siteConfig} = useDocusaurusContext();
+return (
+<header>
+  <section class="herowave">
+    <div class="content">
+      <h2>{siteConfig.title}</h2>
+      <h3>{siteConfig.tagline}</h3>
     </div>
-  <div class="waves"></div>
-</section>
-      </div>
-    </header>
-  );
+    <div class="waves"></div>
+  </section>
+</header>
+);
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+const {siteConfig} = useDocusaurusContext();
+return (
+<Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head/>">
+  <HomepageHeader />
+  <main>
+    <HomepageFeatures />
+  </main>
+</Layout>
+);
 }
