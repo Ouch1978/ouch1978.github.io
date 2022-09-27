89150d1b28ae0300bc6027629f321b7856c1c4bb
Ouch1978 <ouch1978@hotmail.com>
2022-09-27 21:50:17 +0800

    1. Refactor home page.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Achievements from "../components/Achievements";
-import {Hero} from "../components/Hero";
+import { Hero } from "../components/Hero";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`首頁`} description="不務正業的架構師">
+    <Layout title={`首頁`} description={siteConfig.tagline}>
       <Hero />
       <main>
         <Achievements />
