aa4007088e75b37a0a6085916d0d7ee4c85fa681
Ouch1978 <ouch1978@hotmail.com>
2024-08-24 23:48:09 +0800

    1. Disable pangu.js

--- static/assets/enablePangu.js
+++ static/assets/enablePangu.js
@@ -1,7 +1,11 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // listen to any DOM change and automatically perform spacing via MutationObserver()
   //pangu.autoSpacingPage();
-  pangu.spacingElementByTagName('article');
-  pangu.spacingElementByTagName('nav');
-  pangu.spacingElementByClassName('table-of-contents');
+
+    //pangu.spacingElementByTagName('article');
+    //pangu.spacingElementByTagName('nav');
+    //pangu.spacingElementByClassName('table-of-contents');
 });
